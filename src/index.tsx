a29fad5ab6b4efa0763fa37d9ba2dab83d0c727e
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-05-29 00:58:31 +0900

    feat : globalstyle 및 theme 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@emotion/react';
+import GlobalStyle from '@/styles/GlobalStyle';
+import theme from '@/styles/theme';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<ThemeProvider theme={theme}>
+		<GlobalStyle />
+		<App />
+	</ThemeProvider>,
 );
---
+++ src/types/import-emotion.d.ts
@@ -0,0 +1,39 @@
+import '@emotion/react';
+
+declare module '@emotion/react' {
+	export interface Theme {
+		colors: {
+			bt_black: string;
+			bt_deepGray: string;
+			bt_lightGray: string;
+			bt_gray: string;
+			bt_green: string;
+			bt_yellow: string;
+			bt_white: string;
+		};
+		fontSize: {
+			f10: string;
+			f12: string;
+			f13: string;
+			f14: string;
+			f15: string;
+			f16: string;
+			f20: string;
+		};
+		fontWeight: {
+			light: number;
+			normal: number;
+			semiBold: number;
+			bold: number;
+		};
+		lineHeight: {
+			lh10: string;
+			lh12: string;
+			lh13: string;
+			lh14: string;
+			lh15: string;
+			lh16: string;
+			lh20: string;
+		};
+	}
+}
