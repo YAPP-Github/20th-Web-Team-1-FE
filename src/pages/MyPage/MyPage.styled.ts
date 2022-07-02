9100a145b02cc09563faa4fff18a0302d8c42db4
numeru <numeru@naver.com>
2022-07-02 22:59:10 +0900

    Fix : 마이페이지 스타일 수정

--- src/pages/MyPage/MyPage.styled.ts
+++ src/pages/MyPage/MyPage.styled.ts
@@ -1,8 +1,7 @@
 import styled from '@emotion/styled';
 
 export const MyPageContainer = styled.main`
-	min-height: calc(100vh - 85px);
+	min-height: calc(100vh - 65px);
 	background-color: ${({ theme }) => theme.colors.bt_primary[20]};
-	padding-bottom: 50px;
 	margin-bottom: -20px;
 `;
