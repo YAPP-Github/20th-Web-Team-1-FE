1c00683a5d0bd57fb93a4f41558809c6049beedd
numeru <numeru@naver.com>
2022-07-07 13:06:31 +0900

    Feat : 프로필 수정 페이지 스타일링

--- src/components/shared/Routers.tsx
+++ src/components/shared/Routers.tsx
@@ -6,6 +6,7 @@ import TreeAddForm from '../features/Trees/TreeAddForm';
 import MyPage from '@/pages/MyPage/MyPage';
 import MessageDetail from '@/pages/MessageDetail/MessageDetail';
 import MessageSender from '@/pages/MessageSender';
+import ProfileEdit from '@/pages/MyPage/ProfileEdit';
 
 const Routers = () => {
 	return (
@@ -14,6 +15,7 @@ const Routers = () => {
 			<Route path="/trees" element={<Trees />} />
 			<Route path="/trees/edit" element={<TreeAddForm />} />
 			<Route path="/mypage" element={<MyPage />} />
+			<Route path="/mypage/edit" element={<ProfileEdit />} />
 			<Route path="/send-message" element={<MessageSender />} />
 			<Route path="/messages/:messageId" element={<MessageDetail />} />
 
---
+++ src/pages/MyPage/ProfileEdit/ProfileEdit.styled.ts
@@ -0,0 +1,64 @@
+import styled from '@emotion/styled';
+export const ProfileEditForm = styled.form`
+	width: 100%;
+	min-height: calc(100vh - 85px);
+	padding: 154px 32px 30px 32px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: space-between;
+`;
+
+export const ProfileEditWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin-bottom: 30px;
+
+	& > label {
+		font-size: 20px;
+		font-weight: ${({ theme }) => theme.fontWeight.medium};
+		margin-bottom: 101px;
+	}
+
+	& > input {
+		width: 150px;
+		padding: 8px 0;
+		text-align: center;
+		border: none;
+		border-bottom: 2px solid ${({ theme }) => theme.colors.bt_primary[200]};
+		font-weight: ${({ theme }) => theme.fontWeight.medium};
+		font-size: 20px;
+		margin-top: 10px;
+	}
+`;
+
+export const Buttons = styled.div`
+	width: 100%;
+	display: flex;
+	gap: 14px;
+
+	& > a {
+		width: 50%;
+		padding: 17px 0;
+		text-align: center;
+		background-color: ${({ theme }) => theme.colors.bt_grey[20]};
+		color: ${({ theme }) => theme.colors.bt_grey[50]};
+		border-radius: 10px;
+		font-size: 16px;
+		font-weight: ${({ theme }) => theme.fontWeight.bold};
+		line-height: 19.5px;
+	}
+
+	& > button {
+		width: 50%;
+		padding: 17px 0;
+		text-align: center;
+		background-color: ${({ theme }) => theme.colors.bt_primary[200]};
+		color: ${({ theme }) => theme.colors.bt_white};
+		border-radius: 10px;
+		border: none;
+		font-size: 16px;
+		font-weight: ${({ theme }) => theme.fontWeight.bold};
+	}
+`;
