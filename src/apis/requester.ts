92710c8bafc662e386a022f6b2b48a87e01dd0e0
numeru <numeru@naver.com>
2022-07-09 18:44:34 +0900

    Fix : vite 환경 변수 사용 방식 수정

--- src/apis/requester.ts
+++ src/apis/requester.ts
@@ -3,7 +3,7 @@ import { getStorageItem, storageAccessKey } from '@/utils/local-storage';
 
 const createAxiosInstance = () => {
 	const base = axios.create({
-		baseURL: process.env.API_BASE_URL,
+		baseURL: import.meta.env.VITE_API_BASE_URL,
 	});
 
 	base.interceptors.response.use(
