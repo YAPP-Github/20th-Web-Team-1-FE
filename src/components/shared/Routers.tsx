9f98e5f12b58146472d7cc72995e09eda67f9272
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-07-12 02:10:12 +0900

    Fix : 메세지함 페이지 변경

--- src/components/shared/Routers.tsx
+++ src/components/shared/Routers.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import NoticeTree from '@/pages/NoticeTree';
-import Trees from '@/pages/Trees';
+import { NoticeTree, Trees, MyPage, MessageSender, MessageDetail, MessageBox } from '@/pages';
 import TreeAddForm from '../features/Trees/TreeAddForm';
-import MyPage from '@/pages/MyPage/MyPage';
-import MessageDetail from '@/pages/MessageDetail/MessageDetail';
-import MessageSender from '@/pages/MessageSender';
 import ProfileEdit from '@/pages/MyPage/ProfileEdit';
 import Login from '@/pages/Login';
-import Message from '@/pages/Message';
 
 const Routers = () => {
 	return (
@@ -20,7 +15,7 @@ const Routers = () => {
 			<Route path="/mypage" element={<MyPage />} />
 			<Route path="/mypage/edit" element={<ProfileEdit />} />
 			<Route path="/send-message" element={<MessageSender />} />
-			<Route path="/messages" element={<Message />} />
+			<Route path="/messages" element={<MessageBox />} />
 			<Route path="/messages/:messageId" element={<MessageDetail />} />
 			{/* <Route path="/friends" element={} />
 			<Route path="/*" element={} /> */}
