7bac43619e7c254d2908ea84e32f64a04c7736d1
numeru <numeru@naver.com>
2022-07-13 01:25:01 +0900

    Feat : 헤더 로고 추가

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import EmptyProfileImg from '@/assets/images/layout/empty_profile_img.svg';
+import BetreeLogo from '@/assets/images/layout/betree_logo.svg';
 import * as S from './Header.styled';
 
 const Header = () => {
 	return (
 		<S.HeaderWrapper>
-			<img src="" alt="로고" />
+			<Link to="/">
+				<img src={BetreeLogo} alt="로고" />
+			</Link>
 
 			<Link to="/mypage">
 				<img src={EmptyProfileImg} alt="마이페이지" />
