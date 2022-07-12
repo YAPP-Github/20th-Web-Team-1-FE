f331d7bcbdac50ffc0cb43e97cc5e118084cea71
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-07-13 00:47:42 +0900

    feat : 알림나무 데이터 패칭

--- src/components/features/NoticeTree/Tree/index.tsx
+++ src/components/features/NoticeTree/Tree/index.tsx
@@ -3,19 +3,19 @@ import * as S from './index.styled';
 import Apple from '@/assets/images/noticeTree/apple.svg';
 import { TreeProps } from './index.type';
 
-const Tree = ({ removeFruit, messages }: TreeProps) => {
+const Tree = ({ readNoticeMessage, messages }: TreeProps) => {
 	return (
 		<S.TreeWrapper>
 			<S.TreeCircle>
 				<S.InnerCircle>
-					{messages?.map((arr, idx) => {
+					{messages?.map((message, idx) => {
 						return (
 							<S.Fruit
 								key={`noticeTree_fruit_${idx}`}
 								src={Apple}
-								innerWidth={`${arr.width}%`}
-								innerHeight={`${arr.height}%`}
-								onClick={() => removeFruit && removeFruit(idx)}
+								innerWidth={`${message.width}%`}
+								innerHeight={`${message.height}%`}
+								onClick={() => readNoticeMessage(message.id, idx)}
 							/>
 						);
 					})}
