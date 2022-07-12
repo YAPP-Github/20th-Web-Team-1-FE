a009f81bcc124510cf2a12a703c3c265efbd9e4f
numeru <numeru@naver.com>
2022-07-12 22:59:19 +0900

    Feat : 로그아웃 기능 추가

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,3 +1,4 @@
+import { removeStorageItem } from '@/utils/local-storage';
 import API_URL, { POST } from '@/constants/api';
 import { setStorageItem, storageAccessKey } from '@/utils/local-storage';
 import { requester } from './requester';
@@ -19,6 +20,25 @@ export const login = async (kakaoAccessToken: string) => {
 		const betreeAccessToken = headers.authorization.split(' ')[1];
 
 		setStorageItem(storageAccessKey, betreeAccessToken);
+
+		return status;
+	} catch (error) {
+		// 에러 핸들링
+	}
+};
+
+export const logout = async () => {
+	const {
+		auth: { logout },
+	} = API_URL;
+
+	try {
+		const { status } = await requester({
+			method: POST,
+			url: logout,
+		});
+
+		removeStorageItem(storageAccessKey);
 
 		return status;
 	} catch (error) {
