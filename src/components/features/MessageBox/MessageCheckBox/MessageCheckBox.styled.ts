cf1f1936ddfdbd4ab674f00630375f5b6451d994
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-07-13 04:13:52 +0900

    style : 메세지함 체크박스 스타일 변경

--- src/components/features/MessageBox/MessageCheckBox/MessageCheckBox.styled.ts
+++ src/components/features/MessageBox/MessageCheckBox/MessageCheckBox.styled.ts
@@ -13,28 +13,39 @@ export const CheckBoxLabel = styled.label`
 	line-height: ${({ theme }) => theme.lineHeight.lh14};
 	letter-spacing: -0.02em;
 	color: ${({ theme }) => theme.colors.bt_grey[120]};
+
 	cursor: pointer;
 `;
 
 export const HiddenCheckBoxInput = styled.input`
-	position: absolute;
 	width: 1px;
 	height: 1px;
+
+	position: absolute;
+
+	white-space: nowrap;
+
 	margin: -1px;
 	padding: 0;
+
 	border: 0;
+
 	clip: rect(0, 0, 0, 0);
 	clip-path: inset(50%);
+
 	overflow: hidden;
-	white-space: nowrap;
 `;
 
 export const CheckBoxInput = styled.div<CheckedProps>`
 	width: 16px;
 	height: 16px;
+
 	margin-right: 6px;
+
+	background-color: ${({ checked, theme }) => (checked ? theme.colors.bt_primary[200] : 'transparent')};
+
 	border: 0.5px solid ${({ theme }) => theme.colors.bt_primary[200]};
 	border-radius: 2px;
-	background-color: ${({ checked, theme }) => (checked ? theme.colors.bt_primary[200] : 'transparent')};
+
 	cursor: pointer;
 `;
