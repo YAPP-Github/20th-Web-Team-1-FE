7146006d42615ac034da8836970a5b55ae164865
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-07-13 04:14:55 +0900

    feat : 메세지함 관련 컴포넌트 export

--- src/components/features/MessageBox/index.ts
+++ src/components/features/MessageBox/index.ts
@@ -1,3 +1,5 @@
 export { default as MessageCheckBox } from './MessageCheckBox';
 export { default as MessageContent } from './MessageContent';
 export { default as MessageMenu } from './MessageMenu';
+export { default as MakingFruitMenu } from './MakingFruitMenu';
+export { default as BottomButtons } from './BottomButtons';
