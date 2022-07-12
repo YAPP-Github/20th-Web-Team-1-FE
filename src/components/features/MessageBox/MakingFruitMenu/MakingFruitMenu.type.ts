5dd5eb920a4eb169e08d8b67e8774b26e081b4a2
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-07-13 04:14:40 +0900

    feat : 메세지함 열매맺기 상단 메뉴 스타일  및 로직 추가

---
+++ src/components/features/MessageBox/MakingFruitMenu/MakingFruitMenu.type.ts
@@ -0,0 +1,6 @@
+export interface MakingFruitMenuProps {
+	numberOfMessages: number;
+	numberOfCheckedMessages: number;
+	isShownCheckedMessages: boolean;
+	setIsShownCheckedMessages: React.Dispatch<React.SetStateAction<boolean>>;
+}
---
+++ src/components/features/MessageBox/MakingFruitPopup/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import * as S from './MakingFruitPopup.styled';
+import AlertBee from '@/assets/images/noticeTree/alert_bee.svg';
+
+const MakingFruitPopup = () => {
+	return (
+		<S.PopUpWrapper>
+			<S.IconBox>
+				<img src={AlertBee} alt="alertBee" />
+			</S.IconBox>
+
+			<S.MessageBox>
+				<S.NameText>열매를 맺으실 건가요?</S.NameText>
+				<S.AlertMessage>열매 맺을 메세지를 선택해주세요.</S.AlertMessage>
+			</S.MessageBox>
+		</S.PopUpWrapper>
+	);
+};
+
+export default MakingFruitPopup;
