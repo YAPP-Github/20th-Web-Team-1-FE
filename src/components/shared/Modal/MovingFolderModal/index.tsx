600cb0caf68510324071f8f5156a9140153534d5
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-07-14 02:50:35 +0900

    feat : 폴더 이동 모달 ui 수정

--- src/components/shared/Modal/MovingFolderModal/index.tsx
+++ src/components/shared/Modal/MovingFolderModal/index.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ModalFrame from '../ModalFrame';
 import { MovingFolderModalProps } from './MovingFolderModal.type';
 import * as S from './MovingFolderModal.styled';
+import Button from '../../Button';
 
 const MovingFolderModal = ({ isMoving, onToggleMovingFolderModal }: MovingFolderModalProps) => {
+	const [folders, setFolders] = useState(['aaa', 'bbb', 'vvv']);
+	const [selectFolder, setSelectFolder] = useState('');
+
 	return (
 		<ModalFrame onModal={isMoving} setOnModal={onToggleMovingFolderModal}>
 			<S.Wrapper>
@@ -13,13 +17,28 @@ const MovingFolderModal = ({ isMoving, onToggleMovingFolderModal }: MovingFolder
 					</S.MoadlHead>
 					<S.ModalBody>
 						<S.FolderContainer>
-							<S.Folder>aaa</S.Folder>
-							<S.Folder>bbb</S.Folder>
-							<S.Folder>xxx</S.Folder>
+							{folders.map((folder) => (
+								<S.Folder key={folder} onClick={() => setSelectFolder(folder)} isSelected={folder === selectFolder}>
+									{folder}
+								</S.Folder>
+							))}
 						</S.FolderContainer>
 						<S.ModalButtonContainer>
-							<S.ModalButton onClick={onToggleMovingFolderModal}>취소하기</S.ModalButton>
-							<S.MoveButton onClick={() => console.log('move the folder')}>이동하기</S.MoveButton>
+							<S.ModalButton>
+								<Button type="button" bgColor="normal" fontWeight="bold" onClick={onToggleMovingFolderModal}>
+									취소하기
+								</Button>
+							</S.ModalButton>
+							<S.MoveButton>
+								<Button
+									type="button"
+									bgColor="primary"
+									fontWeight="bold"
+									onClick={() => console.log('move the folder')}
+								>
+									이동하기
+								</Button>
+							</S.MoveButton>
 						</S.ModalButtonContainer>
 					</S.ModalBody>
 				</S.ModalInner>
