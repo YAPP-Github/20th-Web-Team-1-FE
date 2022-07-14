f7dfd45c170cc62e9fe2361585a6f2ecf23dcd8d
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-07-15 01:50:53 +0900

    feat : 열매 맺을 메세지 api 연결 및 로직 정리

--- src/components/features/MessageBox/BottomButtons/index.tsx
+++ src/components/features/MessageBox/BottomButtons/index.tsx
@@ -1,15 +1,40 @@
 import React from 'react';
 import * as S from './BottomButtons.styled';
 import { BottomButtonsProps } from './BottomButtons.type';
+import { openMessages } from '@/apis/messages';
+import { useMutation, useQueryClient } from 'react-query';
 
-const BottomButtons = ({ isEdit, isMakingFruit, editMakingToggleHandler }: BottomButtonsProps) => {
+const BottomButtons = ({
+	isEdit,
+	isMakingFruit,
+	editMakingToggleHandler,
+	checkMessages,
+	setShowCheckedMessages,
+}: BottomButtonsProps) => {
+	const queryClient = useQueryClient();
+	const { mutate: openMessagesMutate } = useMutation(() => openMessages(checkMessages), {
+		onSuccess: () => {
+			queryClient.invalidateQueries('getMessages');
+		},
+	});
+
+	const onClickMakeFruitButton = () => {
+		openMessagesMutate();
+		setShowCheckedMessages(false);
+	};
+
+	const onClickBackButton = () => {
+		editMakingToggleHandler('back');
+		setShowCheckedMessages(false);
+	};
+
 	return (
 		<S.BackButtonContainer>
 			{isEdit && <S.LongButton onClick={() => editMakingToggleHandler('back')}>뒤로가기</S.LongButton>}
 			{isMakingFruit && (
 				<>
-					<S.ShortButton onClick={() => editMakingToggleHandler('back')}>뒤로가기</S.ShortButton>
-					<S.ShortGreenButton onClick={() => console.log('save')}>저장하기</S.ShortGreenButton>
+					<S.ShortButton onClick={onClickBackButton}>뒤로가기</S.ShortButton>
+					<S.ShortGreenButton onClick={onClickMakeFruitButton}>저장하기</S.ShortGreenButton>
 				</>
 			)}
 		</S.BackButtonContainer>
