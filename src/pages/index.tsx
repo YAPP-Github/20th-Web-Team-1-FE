bbfac22d5bfbafd221c3b96d2abb30caf27c5d90
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-07-16 01:40:39 +0900

    chore : 메세지 디테일 index.ts로 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,5 @@ export { default as MessageBox } from './MessageBox';
 export { default as NoticeTree } from './NoticeTree';
 export { default as Forest } from './Forest';
 export { default as MessageSender } from './MessageSender';
-export { default as MessageDetail } from './MessageDetail/MessageDetail';
+export { default as MessageDetail } from './MessageDetail';
 export { default as MyPage } from './MyPage/MyPage';
