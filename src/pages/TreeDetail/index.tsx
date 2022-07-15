340720aa05bbd3198f33ba58f02d78252fdf05c7
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-07-16 03:25:08 +0900

    feat : 나무 상세 페이지 UI 및 API 연결

---
+++ src/pages/TreeDetail/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Clouds, Tree, MessageBox, WateringButton } from '@/components/features/NoticeTree';
+import * as S from './TreeDetail.styled';
+
+import useTreeDetail from './useTreeDetail';
+const TreeDetail = () => {
+	const { treeMessages, treeDetailInfo, readTreeMessage, moveTree, showMessage, selectedMessage, setShowMessage } =
+		useTreeDetail();
+
+	return (
+		<>
+			<S.TemporaryWrapper>
+				<Clouds />
+
+				<S.TreeDetailTextWrapper>
+					<S.TreeDetailMainText>
+						{treeDetailInfo?.name === 'DEFAULT' ? '기본폴더' : treeDetailInfo?.name}
+					</S.TreeDetailMainText>
+					<span>맺혀 있는 열매 : {treeDetailInfo?.messages.length}개</span>
+				</S.TreeDetailTextWrapper>
+				<Tree readNoticeMessage={readTreeMessage} messages={treeMessages ? treeMessages : null} />
+
+				{showMessage && (
+					<MessageBox selectedMessage={selectedMessage} showMessageHandler={() => setShowMessage(false)} />
+				)}
+
+				{treeDetailInfo?.prevId !== 0 && (
+					<S.PrevButton onClick={() => moveTree(treeDetailInfo?.prevId)}>{'<'}</S.PrevButton>
+				)}
+				{treeDetailInfo?.nextId !== 0 && (
+					<S.NextButton onClick={() => moveTree(treeDetailInfo?.nextId)}>{'>'}</S.NextButton>
+				)}
+
+				<WateringButton />
+			</S.TemporaryWrapper>
+		</>
+	);
+};
+
+export default TreeDetail;
---
+++ src/pages/TreeDetail/useTreeDetail.ts
@@ -0,0 +1,64 @@
+import { useState, useEffect } from 'react';
+
+import { myInfoState } from '@/stores/user';
+import { useRecoilValue } from 'recoil';
+import { useQuery } from 'react-query';
+import { readTreeDetail } from '@/apis/forest';
+import { useNavigate, useParams } from 'react-router-dom';
+import { MessageWithLocationType } from '../NoticeTree/NoticeTree.type';
+import { readMessage } from '@/apis/messages';
+const useTreeDetail = () => {
+	const { treeId } = useParams();
+
+	const navigate = useNavigate();
+
+	const myInfo = useRecoilValue(myInfoState);
+
+	const userId = myInfo?.id;
+
+	const [treeMessages, setTreeMessages] = useState<MessageWithLocationType[] | null>(null);
+	const [showMessage, setShowMessage] = useState(false);
+	const [selectedMessage, setSelectedMessage] = useState<MessageWithLocationType | null>(null);
+
+	const { data: treeDetailInfo } = useQuery(
+		['readTreeDetail', { treeId: treeId, userId: userId }],
+		() => readTreeDetail({ treeId: treeId, userId: String(userId) }),
+		{ enabled: !!treeId && !!userId },
+	);
+
+	const readTreeMessage = (messageId: number, selectedIdx: number) => {
+		if (treeMessages) {
+			readMessage(messageId);
+			setShowMessage(true);
+
+			setSelectedMessage(treeMessages[selectedIdx]);
+		}
+	};
+
+	const moveTree = (nextTree: number | undefined) => {
+		setShowMessage(false);
+		navigate(`/forest/${nextTree}`);
+	};
+
+	useEffect(() => {
+		if (treeDetailInfo !== undefined) {
+			const newMessages = treeDetailInfo?.messages.map((message) => ({
+				...message,
+				width: Math.floor(Math.random() * 100),
+				height: Math.floor(Math.random() * 100),
+			}));
+			setTreeMessages(newMessages);
+		}
+	}, [treeDetailInfo]);
+	return {
+		treeMessages,
+		treeDetailInfo,
+		readTreeMessage,
+		moveTree,
+		showMessage,
+		selectedMessage,
+		setShowMessage,
+	};
+};
+
+export default useTreeDetail;
