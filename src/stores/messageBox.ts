530b6fb04d9f5e8feabf598c727a175d8e00243a
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-07-16 01:41:02 +0900

    delete : 사용 하지 않는 selector 삭제

--- src/stores/messageBox.ts
+++ src/stores/messageBox.ts
@@ -1,16 +1,7 @@
-import { atom, selector } from 'recoil';
+import { atom } from 'recoil';
 import { MessagesType } from '@/types/message';
 
 export const messagesState = atom<MessagesType | undefined>({
 	key: 'messagesState',
 	default: undefined,
-});
-
-export const getMessagesSelector = selector({
-	key: 'getMessagesSelector',
-	get: ({ get }) => {
-		const messages = get(messagesState);
-
-		return messages;
-	},
 });
