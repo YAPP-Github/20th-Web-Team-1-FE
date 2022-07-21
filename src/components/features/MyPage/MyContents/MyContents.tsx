56eed8b5e60f4505a2c1ea583a16b37511ebbc6b
numeru <numeru@naver.com>
2022-07-21 14:39:36 +0900

    Feat : 주소 복사 기능 추가

--- src/components/features/MyPage/MyContents/MyContents.tsx
+++ src/components/features/MyPage/MyContents/MyContents.tsx
@@ -3,11 +3,11 @@ import { useNavigate } from 'react-router-dom';
 import { logout } from '@/apis/auth';
 import { RESPONSE_SUCCESS_OK } from '@/constants/api';
 import * as S from './MyContents.styled';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { myInfoState } from '@/stores/user';
 
 const MyContents = () => {
-	const setMyInfo = useSetRecoilState(myInfoState);
+	const [myInfo, setMyInfo] = useRecoilState(myInfoState);
 	const navigate = useNavigate();
 
 	const handleClickLogoutButton = async () => {
@@ -19,10 +19,16 @@ const MyContents = () => {
 		}
 	};
 
+	const DOMAIN_URL = 'http://localhost:3000';
+
+	const handleCopyUrlToClipBoard = async () => {
+		await navigator.clipboard.writeText(`${DOMAIN_URL}/forest/${myInfo?.id}`);
+	};
+
 	return (
 		<S.MyContentsContainer>
 			<S.MyMenuLink to="/mypage/edit">프로필 수정하기</S.MyMenuLink>
-			<S.MyMenuButton type="button" color="primary">
+			<S.MyMenuButton type="button" color="primary" onClick={handleCopyUrlToClipBoard}>
 				Url 주소 복사하기
 			</S.MyMenuButton>
 			<S.MyMenuButton type="button" color="grey" onClick={handleClickLogoutButton}>
