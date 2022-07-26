cd711182cc97bf7c8682c2348a69c8a0b8498329
numeru <numeru@naver.com>
2022-07-26 22:04:24 +0900

    Feat : 로그아웃 경고 모달 추가

--- src/components/features/MyPage/MyContents/MyContents.tsx
+++ src/components/features/MyPage/MyContents/MyContents.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { logout } from '@/apis/auth';
 import * as S from './MyContents.styled';
 import { useRecoilState } from 'recoil';
 import { myInfoState } from '@/stores/user';
 import { useMutation } from 'react-query';
+import { AlertModal } from '@/components/shared';
+import LogoutModalImg from '@/assets/images/mypage/logout_bee_img@2x.png';
 
 const MyContents = () => {
 	const [myInfo, setMyInfo] = useRecoilState(myInfoState);
@@ -27,16 +29,30 @@ const MyContents = () => {
 		await navigator.clipboard.writeText(`${DOMAIN_URL}/forest/${myInfo?.id}`);
 	};
 
+	const [isModalVisible, setIsModalVisible] = useState(false);
+
 	return (
-		<S.MyContentsContainer>
-			<S.MyMenuLink to="/mypage/edit">프로필 수정하기</S.MyMenuLink>
-			<S.MyMenuButton type="button" color="primary" onClick={handleCopyUrlToClipBoard}>
-				Url 주소 복사하기
-			</S.MyMenuButton>
-			<S.MyMenuButton type="button" color="grey" onClick={handleClickLogoutButton}>
-				로그아웃하기
-			</S.MyMenuButton>
-		</S.MyContentsContainer>
+		<>
+			<S.MyContentsContainer>
+				<S.MyMenuLink to="/mypage/edit">프로필 수정하기</S.MyMenuLink>
+				<S.MyMenuButton type="button" color="primary" onClick={handleCopyUrlToClipBoard}>
+					Url 주소 복사하기
+				</S.MyMenuButton>
+				<S.MyMenuButton type="button" color="grey" onClick={() => setIsModalVisible(true)}>
+					로그아웃하기
+				</S.MyMenuButton>
+			</S.MyContentsContainer>
+			{isModalVisible && (
+				<AlertModal
+					isOpen={isModalVisible}
+					modalMainImage={LogoutModalImg}
+					buttonTitle="로그아웃하기"
+					modalDescMessages={['로그아웃하시겠습니까?', '곧 다시 만나요...!']}
+					handleCloseBtnClick={() => setIsModalVisible(false)}
+					handleMainBtnClick={handleClickLogoutButton}
+				/>
+			)}
+		</>
 	);
 };
 
