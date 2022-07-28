3bf5ad0ebba5af5e4a5565543355698183eb6984
youngmin wi <youngminieo1005@gmail.com>
2022-07-28 23:52:31 +0900

    Feat: 물 주기 글자 수 제한 alert modal 추가

--- src/pages/MessageSender/index.tsx
+++ src/pages/MessageSender/index.tsx
@@ -3,7 +3,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import * as S from './MessageSender.styled';
 import { RecipientName, FolderSelect, MessageInput, AnonymousCheckBox } from '@/components/features/MessageSender';
 import Button from '@/components/shared/Button';
-import { SuccessModal } from '@/components/shared';
+import { MediumAlertModal, SuccessModal } from '@/components/shared';
 import { useQuery, useMutation } from 'react-query';
 import { useRecoilValue } from 'recoil';
 import { myInfoState } from '@/stores/user';
@@ -11,6 +11,7 @@ import { getForest } from '@/apis/forest';
 import { DEFAULT_FOLDER_NAME } from '@/constants/messageSender';
 import { Folder } from '@/types/forest';
 import { postMessages } from '@/apis/messages';
+import PencilImg from '@/assets/images/shared/pencil.png';
 
 const MessageSender = () => {
 	const navigate = useNavigate();
@@ -31,9 +32,10 @@ const MessageSender = () => {
 	const messageInputRef = useRef<HTMLTextAreaElement>(null);
 	const [recipientName, setRecipientName] = useState('나에게');
 	const [checkAnonymous, setCheckAnonymous] = useState(false);
-	const [isSucceedSendMessage, setIsSucceedSendMessage] = useState(false);
 	const [selectedFolder, setSelectedFolder] = useState(DEFAULT_FOLDER_NAME);
 	const [isOpenedSelectFolderBox, setIsOpenedSelectedFolderBox] = useState(false);
+	const [isSucceedSendMessage, setIsSucceedSendMessage] = useState(false);
+	const [isMessageTextSizeAlertVisible, setIsMessageTextSizeAlertVisible] = useState(false);
 
 	const handleSelectedFolderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		const selected = event.target.value;
@@ -53,6 +55,13 @@ const MessageSender = () => {
 		event.preventDefault();
 
 		const data = getPostingMessageInfo();
+
+		if (data.content.length < 10 || data.content.length > 1000) {
+			console.log(data.content.length);
+			setIsMessageTextSizeAlertVisible(true);
+			return;
+		}
+
 		postMessageMutation.mutate(data);
 	};
 
@@ -77,39 +86,50 @@ const MessageSender = () => {
 	}, [trees, treeId]);
 
 	return (
-		<S.MessageSenderContainer onSubmit={handleSubmitMessage}>
-			<S.TopWrapper>
-				<RecipientName name={recipientName} />
-				<FolderSelect
-					folders={trees}
-					isOpenedFolderBox={isOpenedSelectFolderBox}
-					onToggleSelectedFolderBox={onToggleSelectedFolderBox}
-					selectedFolder={selectedFolder}
-					handleSelectedFolderChange={handleSelectedFolderChange}
-				/>
-			</S.TopWrapper>
+		<S.Container>
+			<S.Background />
+			<S.MessageSenderContainer onSubmit={handleSubmitMessage}>
+				<S.TopWrapper>
+					<RecipientName name={recipientName} />
+					<FolderSelect
+						folders={trees}
+						isOpenedFolderBox={isOpenedSelectFolderBox}
+						onToggleSelectedFolderBox={onToggleSelectedFolderBox}
+						selectedFolder={selectedFolder}
+						handleSelectedFolderChange={handleSelectedFolderChange}
+					/>
+				</S.TopWrapper>
 
-			<S.MessageInputWrapper>
-				<MessageInput messageInputRef={messageInputRef} />
-			</S.MessageInputWrapper>
+				<S.MessageInputWrapper>
+					<MessageInput messageInputRef={messageInputRef} />
+				</S.MessageInputWrapper>
 
-			<S.AnonymousCheckBoxWrapper>
-				<AnonymousCheckBox checked={checkAnonymous} handleToggleChecked={onToggleCheckAnonymous} />
-			</S.AnonymousCheckBoxWrapper>
+				<S.AnonymousCheckBoxWrapper>
+					<AnonymousCheckBox checked={checkAnonymous} handleToggleChecked={onToggleCheckAnonymous} />
+				</S.AnonymousCheckBoxWrapper>
 
-			<S.ButtonWrapper>
-				<Button type="button" bgColor="normal" fontWeight="medium" onClick={onGoBackClick}>
-					뒤로가기
-				</Button>
-				<Button type="submit" bgColor="primary" fontWeight="bold">
-					물 주기
-				</Button>
-			</S.ButtonWrapper>
+				<S.ButtonWrapper>
+					<Button type="button" bgColor="normal" fontWeight="medium" onClick={onGoBackClick}>
+						뒤로가기
+					</Button>
+					<Button type="submit" bgColor="primary" fontWeight="bold">
+						물 주기
+					</Button>
+				</S.ButtonWrapper>
 
-			{isSucceedSendMessage && (
-				<SuccessModal isSucceedSendMessage={isSucceedSendMessage} handleCloseBtnClick={onGoBackClick} />
-			)}
-		</S.MessageSenderContainer>
+				{isSucceedSendMessage && (
+					<SuccessModal isSucceedSendMessage={isSucceedSendMessage} handleCloseBtnClick={onGoBackClick} />
+				)}
+				{isMessageTextSizeAlertVisible && (
+					<MediumAlertModal
+						image={PencilImg}
+						contents={['10자 이상, 1000자 이하로', '메시지를 작성해야 해요!']}
+						buttonText={'닫기'}
+						modalHandler={() => setIsMessageTextSizeAlertVisible(false)}
+					/>
+				)}
+			</S.MessageSenderContainer>
+		</S.Container>
 	);
 };
 
