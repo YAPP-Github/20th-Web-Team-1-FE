9930fdc281afcc47859f99029e3a78590e3556f1
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-07-29 00:21:47 +0900

    fix : 메세지 박스 크기 조절

--- src/components/features/NoticeTree/MessageBox/MessageBox.styled.ts
+++ src/components/features/NoticeTree/MessageBox/MessageBox.styled.ts
@@ -1,9 +1,23 @@
 import styled from '@emotion/styled';
 import { DefaultButton } from '@/components/shared/DefaultButton/DefaultButton.styled';
+
+export const MessageBoxWrapper = styled.div`
+	position: absolute;
+	bottom: 45px;
+	width: 100%;
+	min-height: 173px;
+	max-height: 350px;
+	z-index: 1;
+	display: flex;
+	justify-content: center;
+
+	padding: 0px 40px;
+`;
+
 export const MessageBoxInner = styled.div`
 	width: 100%;
 	height: 100%;
-	margin: auto 40px;
+	min-height: 173px;
 	padding: 24px 27px;
 	background: ${({ theme }) => theme.colors.bt_white};
 	box-sizing: border-box;
@@ -13,16 +27,6 @@ export const MessageBoxInner = styled.div`
 	display: flex;
 	flex-direction: column;
 	justify-content: space-between;
-`;
-
-export const MessageBoxWrapper = styled.div`
-	position: absolute;
-	bottom: 45px;
-	width: 100%;
-	height: 173px;
-	z-index: 1;
-	display: flex;
-	justify-content: center;
 `;
 
 export const CancelButton = styled(DefaultButton)`
@@ -46,15 +50,16 @@ export const MessageTitle = styled.span`
 `;
 
 export const MessageText = styled.p`
+	flex-grow: 1;
 	font-weight: ${({ theme }) => theme.fontWeight.normal};
 	font-size: ${({ theme }) => theme.fontSize.f14};
 	line-height: ${({ theme }) => theme.lineHeight.lh22};
 	color: ${({ theme }) => theme.colors.bt_grey[40]};
-	height: 80%;
-	overflow-y: scroll;
-	::-webkit-scrollbar {
+	overflow-y: auto;
+	overflow-x: hidden;
+	/* ::-webkit-scrollbar {
 		display: none;
-	}
+	} */
 `;
 
 export const MessageWriter = styled.p`
@@ -62,10 +67,11 @@ export const MessageWriter = styled.p`
 	font-size: ${({ theme }) => theme.fontSize.f12};
 	line-height: ${({ theme }) => theme.lineHeight.lh12};
 	color: ${({ theme }) => theme.colors.bt_grey[55]};
+	word-break: break-all;
 `;
 
 export const MessageWriterWrapper = styled.div`
-	height: 10%;
+	padding-top: 5px;
 	display: flex;
 	justify-content: flex-end;
 	align-items: center;
