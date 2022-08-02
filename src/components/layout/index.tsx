4bfb4b79c4790d1d1d622c18eed6ba4f97c66ffb
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-08-03 01:03:33 +0900

    fix : 퍼블릭 사용 바텀 추가

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,4 +1,5 @@
+export { default as BackgroundArea } from './BackgroundArea';
+export { default as BottomNav } from './BottomNav/BottomNav';
 export { default as Header } from './Header/Header';
 export { default as Layout } from './Layout/Layout';
-export { default as BottomNav } from './BottomNav/BottomNav';
-export { default as BackgroundArea } from './BackgroundArea';
+export { default as PublicBottomNav } from './PublicBottomNav';
