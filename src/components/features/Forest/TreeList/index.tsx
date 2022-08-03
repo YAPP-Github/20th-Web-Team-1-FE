fc20b62a92ec34450dd3a1e2e33c4bd06da24cd5
youngmin wi <youngminieo1005@gmail.com>
2022-08-04 00:41:00 +0900

    Fix: 외부인 나무 숲 방문 시, 나무 추가 버튼 제공 유무 분기처리

--- src/components/features/Forest/TreeList/index.tsx
+++ src/components/features/Forest/TreeList/index.tsx
@@ -4,16 +4,23 @@ import TreeItemAdd from './TreeItemAdd';
 import * as S from './TreeList.styled';
 import { TREE_SIZE_MAX } from '@/constants/forest';
 import { Props } from './TreeList.type';
+import { useParams } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { myInfoState } from '@/stores/user';
 
 const TreeList = ({ trees }: Props) => {
+	const myInfo = useRecoilValue(myInfoState);
+	const { userId: currentforestUserId } = useParams();
+
 	const checkTreeSizeMax = () => trees && trees?.length < TREE_SIZE_MAX;
+	const isVisitedMyForest = () => Number(currentforestUserId) === myInfo?.id;
 
 	return (
 		<S.TreeItemList>
 			{trees?.map((tree) => {
 				return <TreeItem key={tree.id} tree={tree} />;
 			})}
-			{checkTreeSizeMax() && <TreeItemAdd />}
+			{!currentforestUserId ? checkTreeSizeMax() && <TreeItemAdd /> : isVisitedMyForest() && <TreeItemAdd />}
 		</S.TreeItemList>
 	);
 };
