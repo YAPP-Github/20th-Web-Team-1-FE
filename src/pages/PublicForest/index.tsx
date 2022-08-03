63712aa1476eab38c3c5f5128254c6ad49338fe5
youngmin wi <youngminieo1005@gmail.com>
2022-08-04 02:11:37 +0900

    Fix: 본인 나무 숲이 비었을 때, 본인 로그인 후 공유 URL 로 접근했을 경우는 나무 추가 버튼이 보이도록 수정

--- src/pages/PublicForest/index.tsx
+++ src/pages/PublicForest/index.tsx
@@ -17,18 +17,22 @@ const PublicForest = () => {
 
 	const myInfo = useRecoilValue(myInfoState);
 
-	const { userId } = useParams();
+	const { userId: currentForestUserId } = useParams();
 
 	const [treeFolders, setTreeFolders] = useState<Folder[] | undefined>(undefined);
 	const [treeOwner, setTreeOwner] = useState('');
 
-	const { data: folders } = useQuery<ForestTrees | undefined>(['getForest', userId], () => getForest(userId), {
-		enabled: !!userId,
-		retry: 0,
-		onError: () => {
-			navigate('not-found');
+	const { data: folders } = useQuery<ForestTrees | undefined>(
+		['getForest', currentForestUserId],
+		() => getForest(currentForestUserId),
+		{
+			enabled: !!currentForestUserId,
+			retry: 0,
+			onError: () => {
+				navigate('not-found');
+			},
 		},
-	});
+	);
 
 	useEffect(() => {
 		if (folders) {
@@ -38,7 +42,7 @@ const PublicForest = () => {
 	}, [folders]);
 
 	useEffect(() => {
-		if (!userId) {
+		if (!currentForestUserId) {
 			navigate('/forest');
 		}
 	}, []);
@@ -50,20 +54,24 @@ const PublicForest = () => {
 				<S.TreesContainer>
 					<MessageChip message={`오늘 하루도 고생한 ${treeOwner}님에게 따듯한 칭찬을 남겨보세요!`} />
 
-					{treeFolders && treeFolders?.length > 0 ? (
-						<S.TreeListBox>
-							<TreeList trees={treeFolders} />
-						</S.TreeListBox>
-					) : (
-						<S.EmptyTreeListBox>
-							<S.EmptyTreeListMessageImg src={SadBeeImg} alt="빈 나무 숲 알림" />
-							<S.EmptyTreeListMessage>{treeOwner} 님이 새로 만든 나무가 존재하지 않습니다.</S.EmptyTreeListMessage>
-							<S.EmptyTreeListMessage>나무에 물 주기를 눌러서 칭찬을 남겨보세요 !</S.EmptyTreeListMessage>
-						</S.EmptyTreeListBox>
-					)}
+					<S.TreeListBox>
+						<TreeList trees={treeFolders} />
+
+						{!myInfo && treeFolders && treeFolders?.length <= 0 && (
+							<S.EmptyTreeListBox>
+								<S.EmptyTreeListMessageImg src={SadBeeImg} alt="빈 나무 숲 알림" />
+								<S.EmptyTreeListMessage>{treeOwner} 님이 새로 만든 나무가 존재하지 않습니다.</S.EmptyTreeListMessage>
+								<S.EmptyTreeListMessage>나무에 물 주기를 눌러서 칭찬을 남겨보세요 !</S.EmptyTreeListMessage>
+							</S.EmptyTreeListBox>
+						)}
+					</S.TreeListBox>
 
 					<S.ButtonBox>
-						<Button type="button" bgColor="primary" onClick={() => navigate(`/message/edit/${userId}/${treeOwner}`)}>
+						<Button
+							type="button"
+							bgColor="primary"
+							onClick={() => navigate(`/message/edit/${currentForestUserId}/${treeOwner}`)}
+						>
 							나무에 물 주기
 						</Button>
 					</S.ButtonBox>
